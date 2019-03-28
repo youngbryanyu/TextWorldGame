fc73f9ac16799d7af13d48bdd08b8b987d41d33d
young <youngyu2002@gmail.com>
2019-03-28 12:30:40 -0700

    added Items into level class

--- src/Level.java
+++ src/Level.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Level {
@@ -32,21 +33,23 @@ public class Level {
     public class Room {
         private String name, description;
         private HashMap<String, Room> neighbors;
+        ArrayList<Item> items;
 
         private Room(String name, String description) {
             this.name = name;
             this.description = description;
             neighbors = new HashMap<>();
+            items = new ArrayList<>();
         }
 
-        private void addNeighbor(Room n) {
+        public void addNeighbor(Room n) {
             neighbors.put(n.getName(), n);
         }
 
         public String getNeighborNames() {
             String names = "";
             for (Room n : neighbors.values()) {
-                names += "-> "+ n.getName() + " >> " + n.getDescription() + "\n";
+                names += "-> " + n.getName() + " >> " + n.getDescription() + "\n";
             }
             if (names.length() > 1) return names.substring(0, names.length() - 1);
             return "";
@@ -66,6 +69,50 @@ public class Level {
 
         public void setDescription(String description) {
             this.description = description;
+        }
+
+        public ArrayList<Item> getItems() {
+            return items;
+        }
+
+        public String displayItems() {
+            String items = "";
+            for (Item e : this.items) {
+                items += e.getName() + " >> " + e.getDescription() + "\n";
+            }
+            if (items.length() > 1) return items.substring(0, items.length() - 1);
+            return items;
+        }
+
+        public void addItem(String name) {
+            items.add(new Item(name, ""));
+        }
+
+        public void addItemn(String name, String description) {
+            items.add(new Item(name, description));
+        }
+
+        public void addItem(Item item) {
+            items.add(item);
+        }
+
+        public Item removeItem(String name) {
+            for (Item item : items) {
+                if (item.getName().equals(name)) {
+                    items.remove(item);
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public boolean destroyItem(String name) {
+            for (Item item : items) {
+                if (item.getName().equals(name)) {
+                    return items.remove(item);
+                }
+            }
+            return false;
         }
     }
 }
