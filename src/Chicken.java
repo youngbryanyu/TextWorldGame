5c511a4f899ddc4e64fb4ae0f62b1f40c78717cc
young <youngyu2002@gmail.com>
2019-04-05 09:52:51 -0700

    fixed chickens

--- src/Chicken.java
+++ src/Chicken.java
@@ -9,19 +9,15 @@ public class Chicken extends Creature {
         this.currentRoom = currentRoom;
     }
 
-    public void move(Level.Room nextRoom) {
-        this.currentRoom = nextRoom;
+    public void move() {
+        moveRandom();
     }
 
-    public void randomizeRoom() {
+    public void moveRandom() {
         HashMap<String, Level.Room> neighbors = currentRoom.getNeighbors();
         ArrayList<Level.Room> neighborList = new ArrayList<>(neighbors.values());
         int index = (int) (Math.random() * neighborList.size());
         if (neighborList.size() == 0) return;
-        move(neighborList.get(index));
-    }
-
-    public Level.Room getCurrentRoom() {
-        return currentRoom;
+        this.currentRoom = neighborList.get(index);
     }
 }
