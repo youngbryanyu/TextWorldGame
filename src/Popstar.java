b03c67931ca03d5d773832645be86b78494142c0
young <youngyu2002@gmail.com>
2019-04-08 22:25:03 -0700

    removed arraylist

--- src/Popstar.java
+++ src/Popstar.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Popstar extends Creature{
+public class Popstar extends Creature {
     private Player player;
 
     public Popstar(Level.Room currentRoom, Player player) {
@@ -9,29 +9,25 @@ public class Popstar extends Creature{
         this.player = player;
     }
 
-    public void move() {
+    public void act() {
         HashMap<String, Level.Room> neighbors = currentRoom.getNeighbors();
         if (neighbors.containsValue(player.getCurrentRoom())) {
             moveTowardsPlayer();
         } else {
-            moveRandom();
+            getRandomNeighbor();
         }
     }
 
-    public void moveRandom() {
-        HashMap<String, Level.Room> neighbors = currentRoom.getNeighbors();
-        ArrayList<Level.Room> neighborList = new ArrayList<>(neighbors.values());
-        int index = (int) (Math.random() * neighborList.size());
-        if (neighborList.size() == 0) return;
-        setRoom(neighborList.get(index));
+    public Level.Room getRandomNeighbor() {
+        return currentRoom.getRandomNeighbor();
     }
 
-   public void moveTowardsPlayer(){
-       ArrayList<Level.Room> nonPlayerNeighbors = currentRoom.getPlayerContainingNeighbors(player);
-       int index = (int) (Math.random() * nonPlayerNeighbors.size());
-       if (nonPlayerNeighbors.size() == 0) return;
-       setRoom(nonPlayerNeighbors.get(index));
-   }
+    public void moveTowardsPlayer() {
+        ArrayList<Level.Room> nonPlayerNeighbors = currentRoom.getPlayerContainingNeighbors(player);
+        int index = (int) (Math.random() * nonPlayerNeighbors.size());
+        if (nonPlayerNeighbors.size() == 0) return;
+        moveToRoom(nonPlayerNeighbors.get(index));
+    }
 
 
 }
